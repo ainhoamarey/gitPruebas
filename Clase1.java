670eed6e0f6e79bf5dd6f0fae54a12acdfe3ef43
ainhoamarey <ainhoamareygonzalez@gmail.com>
2024-03-06 16:50:36 +0100

    Cambios en clase1 y clase2 - Instantánea 1

---
+++ Clase1.java
@@ -0,0 +1,12 @@
+package clases;
+
+public class Clase1 {
+	// clase1.java - Instantánea 1
+	    public void metodo1() {
+	        System.out.println("Método 1 en clase1 - Instantánea 1");
+	    }
+	}
+
+
+
+
---
+++ Clase2.java
@@ -0,0 +1,8 @@
+package clases;
+
+public class Clase2 {
+public static void main(String[] args) {
+	 int j;
+	 int k;
+}
+}
