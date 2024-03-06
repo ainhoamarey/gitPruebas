4a99f3a32b8210cab6012addcf1f36b1006ec63b
ainhoamarey <ainhoamareygonzalez@gmail.com>
2024-03-06 17:19:15 +0100

    modificar metodo en clase2 en rama

--- Clase2.java
+++ Clase2.java
@@ -4,5 +4,11 @@ public class Clase2 {
 public static void main(String[] args) {
 	 int j;
 	 int k;
+	
+	     public void metodoModificado() {
+	         System.out.println("Método modificado en clase2 en la nueva_rama");
+	     }
+	 }
+
 }
 }
