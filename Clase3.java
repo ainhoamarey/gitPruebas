38a7c1cb8106486d904ddc33e0951712bc6f21f6
ainhoamarey <160491039+ainhoamarey@users.noreply.github.com>
2024-03-20 17:14:57 +0100

    Update Clase3.java
    1commit en clase3

--- Clase3.java
+++ Clase3.java
@@ -4,5 +4,6 @@ public class Clase3 {
 	public void nuevaFuncionalidadEnRama() {
         System.out.println("Nueva funcionalidad en clase3 en la nueva_rama");
     }
+	 System.out.println("1 commit");
 
 }
